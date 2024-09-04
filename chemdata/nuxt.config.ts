7237236a4b11fdc9d3c3d31c418d5aa40a1777e5
brendaferrari <brendafferrari@gmail.com>
2024-09-04 11:40:45 +0300

    feat: chemdata static

---
+++ chemdata/nuxt.config.ts
@@ -0,0 +1,42 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  app: { 
+    baseURL: "/brendaferrari.github.io/chemdata",
+    pageTransition: {name: 'page', mode: 'out-in'},
+    head: {
+      link: [{ rel: 'icon', type: 'image/png', href: '/favicon.png' }]
+  }
+
+  },
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/color-mode',
+    '@nuxt/content'
+  ],
+  content: { 
+    highlight: { 
+      langs: ['python'],
+      theme: { 
+        default: 'min-light',
+        dark: 'min-dark'
+      } 
+    }
+  },
+  css: ['~/assets/styles/main.css'],
+  colorMode: {
+    classSuffix: ''
+  },
+  nitro: {
+    prerender: {
+      routes: ['/sitemap.xml']
+    }
+  },
+  generate:{
+    nojekyll: true, //not working on this version
+    fallback: '404.html',
+  },
+  ssr: false, //When false
+  target: "static", // and static, nuxt generates a SPA
+  
+})
